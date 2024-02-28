836d7d982b5a1e76e92b607db0ef09a286171bb4
code_fusion <codefusions.work@gmail.com>
2024-02-28 18:44:12 +0500

    defalt toggle fixed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const toggleList = (heading, ToggleUl) => {
     const ul = document.getElementById(ToggleUl);
     if (ul.style.height != '200px') {
-        ul.style.border = '1px solid #ddd'
+        ul.classList.add('border')
         ul.style.height = '200px'
     } else {
         ul.style.height = '0'
-        ul.style.border = 'none'
+        ul.classList.remove('border')
     }
     if (heading.children[0].style.transform != 'rotate(180deg)') {
         heading.children[0].style.transform = 'rotate(180deg)';
